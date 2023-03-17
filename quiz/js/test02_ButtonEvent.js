aa76e7223e0706ae2649882b13da51f542ed4be4
stonecat <apink191550@gmail.com>
2023-03-17 23:58:34 +0900

    Quiz make calculate

---
+++ quiz/js/test02_ButtonEvent.js
@@ -0,0 +1,32 @@
+const buttons_num = document.querySelectorAll(".Number_input div");
+const buttons_ops = document.querySelectorAll(".Operator_input div");
+
+let isDisabled = false;
+
+buttons_num.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!isDisabled) {
+            Input_Number(button.getAttribute("value"));
+            button.disabled = true;
+            isDisabled = true;
+            setTimeout(() => {
+                button.disabled = false;
+                isDisabled = false;
+            }, 100);
+        }
+    });
+});
+
+buttons_ops.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!isDisabled) {
+            Input_Operator(button.getAttribute("value"));
+            button.disabled = true;
+            isDisabled = true;
+            setTimeout(() => {
+                button.disabled = false;
+                isDisabled = false;
+            }, 100);
+        }
+    })
+})
---
+++ quiz/js/test02_caculate.js
@@ -0,0 +1,139 @@
+let number = document.getElementById("calculator_data");
+
+let num01 = null;
+let num02 = null;
+let work = null;
+
+function reset_data() {
+    number.value = "";
+}
+
+function reset_num() {
+    num01 = null;
+    num02 = null;
+}
+
+function Input_Value(num) {
+    if((typeof num) == "number") {
+        number.value += String(num);
+        console.log(number.value);
+    }
+    else if ((typeof num) == "string") {
+        number.value += num;
+        console.log(number.value);
+    }
+    else {
+        console.log(`ERROR[func Input_Value]>> Type Error`);
+    }
+    
+}
+
+function Check_Num(value) {
+    work = value;
+    if (num01 == null) {
+        num01 = number.value;
+        reset_data();
+    }
+    else if (num01 != null) {
+        num02 = number.value;
+        num01 = calculator(work);
+        reset_data();
+    }
+    else {
+        console.log(`ERROR[func Check_Num]>> num01 Value Error`);
+    }
+}
+
+function calculator(work) {
+    switch(work) {
+        case '+':
+            return parseInt(num01) + parseInt(num02);
+        case '-':
+            return parseInt(num01) - parseInt(num02);
+        case '/':
+            return parseInt(num01) / parseInt(num02);
+        case '%':
+            return parseInt(num01) % parseInt(num02);
+        case '*':
+            return parseInt(num01) * parseInt(num02);
+        default:
+            console.log(`ERROR[func calculator]>> Operator Error`);
+    }
+}
+
+function Input_Number(value) {
+    switch(value) {
+        case "1":
+            Input_Value(1);
+            break;
+        case "2":
+            Input_Value(2);
+            break;
+        case "3":
+            Input_Value(3);
+            break;
+        case "4":
+            Input_Value(4);
+            break;
+        case "5":
+            Input_Value(5);
+            break;
+        case "6":
+            Input_Value(6);
+            break;
+        case "7":
+            Input_Value(7);
+            break;
+        case "8":
+            Input_Value(8);
+            break;
+        case "9":
+            Input_Value(9);
+            break;
+        case "0":
+            Input_Value(0);
+            break;
+        case "00":
+            Input_Value("00");
+            break;
+        case ".":
+            Input_Value(".");
+            break;
+        default:
+            console.log(`ERROR[func Input_Number]>> Button Input Error`);
+            break;
+    }
+}
+
+function Input_Operator(value) {
+    console.log(value);
+    switch(value) {
+        case "%":
+            Check_Num(value);
+            break;
+        case "RS":
+            reset_data();
+            reset_num();
+            break;
+        case "X":
+            Check_Num(value);
+            break;
+        case "/":
+            Check_Num(value);
+            break;
+        case "+":
+            Check_Num(value);
+            break;
+        case "-":
+            Check_Num(value);
+            break;
+        case "=":
+            num02 = number.value;
+            number.value = calculator(work);
+            reset_num();
+            break;
+        default:
+            console.log(`ERROR[func Input_Operator]>> Button Input Error`);
+            break;
+    }
+}
