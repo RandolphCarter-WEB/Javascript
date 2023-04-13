8bdf1d65884188c85492417ab8be189d737890d1
stonecat <apink191550@gmail.com>
2023-04-14 00:26:51 +0900

    code

--- class/code94_regexp02.js
+++ class/code94_regexp02.js
@@ -2,3 +2,10 @@ const target = "This is a pen";
 const regtext = /Is/ig;
 
 console.log(regtext.test(target));
+console.log(regtext.exec(target));
+
+console.log(target.match(regtext));
+console.log(target.replace(regtext, "are"));
+
+console.log(target.search(regtext));
+console.log(target.split(regtext));
---
+++ class/code95_symbol.js
@@ -0,0 +1,3 @@
+const sym1 = symbol("OST");
+console.log(typeof sym1);
+console.log(sym1);
